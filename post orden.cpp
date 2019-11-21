eafbbc242d7cb5d5aa8904be9cab731e4036929a
leon3344 <54146557+leon3344@users.noreply.github.com>
2019-11-21 10:11:54 -0600

    post orden
    este programa es para el arbol en post orden

---
+++ post orden.cpp
@@ -0,0 +1,80 @@
+# include <iostream>
+#include <cstdlib>
+using namespace std;
+
+struct nodo{
+     int nro;
+     struct nodo *izq, *der;
+};
+
+typedef struct nodo *ABB;
+
+
+ABB crearNodo(int x)
+{
+     ABB nuevoNodo = new(struct nodo);
+     nuevoNodo->nro = x;
+     nuevoNodo->izq = NULL;
+     nuevoNodo->der = NULL;
+
+     return nuevoNodo;
+}
+void insertar(ABB &arbol, int x)
+{
+     if(arbol==NULL)
+     {
+           arbol = crearNodo(x);
+     }
+     else if(x < arbol->nro)
+          insertar(arbol->izq, x);
+     else if(x > arbol->nro)
+          insertar(arbol->der, x);
+}
+
+
+
+
+void postOrden(ABB arbol)
+{
+     if(arbol!=NULL)
+     {
+          postOrden(arbol->izq);
+          postOrden(arbol->der);
+          cout << arbol->nro << " ";
+     }
+}
+
+
+int main()
+{
+    ABB arbol = NULL;   
+
+    int n; 
+    int x; 
+
+    cout << "\n\t\t  ..[ ARBOL BINARIO DE BUSQUEDA ]..  \n\n";
+
+    cout << " Numero de nodos del arbol:  ";
+    cin >> n;
+    cout << endl;
+
+    for(int i=0; i<n; i++)
+    {
+        cout << " Numero del nodo " << i+1 << ": ";
+        cin >> x;
+        insertar( arbol, x);
+    }
+
+
+
+    cout << "\n Recorridos del ABB";
+
+
+    cout << "\n\n Post Orden :  ";   postOrden(arbol);
+
+    cout << endl << endl;
+
+    system("pause");
+    return 0;
+}
+
